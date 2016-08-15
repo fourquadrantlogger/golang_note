8ba86637806f83fe1e33cf718f01a90106dd17e8
timeloveboy <734991033@qq.com>
2016-08-15 14:41:09 +0800

    ~

---
+++ go第三方/SummerApp.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/cocotyty/summer"
+	"fmt"
+)
+
+func init() {
+	summer.Toml(`
+    [printer]
+    prefix="[PRINT]"`)
+	summer.Put(&A{})
+	summer.Add("lay", &B{})
+	summer.Put(&Cat{})
+	summer.Put(&Printer{})
+	summer.Start()
+}
+
+func main() {
+	a := summer.GetStoneWithName("a").(*A)
+	a.Call()
+}
+
+type A struct {
+	// $ means you want to get a stone's field , it happened usually after stones inited
+	BoyName string `sm:"$.lay.Name"`
+	B       *B `sm:"lay"`
+	// yes,we support interface ,tag is stone's name
+	C       C `sm:"cat"`
+}
+
+func (a *A)Call() {
+	a.C.Print()
+	fmt.Println("hi ,I am A", "bodys name:", a.BoyName)
+	fmt.Println(a.B)
+}
+
+type B struct {
+	Name string
+}
+
+func (this *B)Init() {
+	this.Name = "Boy!"
+}
+
+type C interface {
+	Print()
+}
+type Printer struct {
+	// if you already set the toml plugin config, you can use the #  ,to get value from toml,
+	// # is toml plugin's name
+	// toml plugin will work after directly dependency resolved,before init
+	Prefix string `sm:"#.printer.prefix"`
+}
+
+func (printer *Printer)Print(str string) {
+	fmt.Println(printer.Prefix + str)
+}
+
+type Cat struct {
+	// * is mostly used tag,summer will find by the field's name  or the field's type or both
+	Printer *Printer `sm:"*"`
+}
+
+func (c *Cat)Ready() {
+	fmt.Println("my name is cat,i am ready.")
+}
+func (c *Cat)Print() {
+	c.Printer.Print("Little Cat")
+}
---
+++ 基本语言知识/错误捕获/recover.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	defer func(){ // 必须要先声明defer，否则不能捕获到panic异常
+		fmt.Println("c")
+		if err:=recover();err!=nil{
+			fmt.Println(err) // 这里的err其实就是panic传入的内容，55
+		}
+		fmt.Println("d")
+	}()
+	f_recover()
+}
+
+func f_recover(){
+	fmt.Println("a")
+	panic(55)
+	fmt.Println("b")
+	fmt.Println("f")
+}
