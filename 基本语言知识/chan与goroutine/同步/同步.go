f092d6fbd2bfa821ffc365687216202b094a0854
timeloveboy <734991033@qq.com>
2016-08-17 20:24:43 +0800

    ~

--- 基本语言知识/chan与goroutine/同步/同步.go
+++ 基本语言知识/chan与goroutine/同步/同步.go
@@ -3,19 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("1")
 	c := make(chan int)  // Allocate a channel.
-
 	// Start the sort in a goroutine; when it completes, signal on the channel.
-
 	go func() {
-
-		fmt.Println("c")
-
+		fmt.Println("2")
 		c <- 1  // Send a signal; value does not matter.
-
+		fmt.Println("3")
 	}()
-
-	doSomethingForAWhile()
-
-	<-c   // Wait for sort to finish; discard sent value.
+	fmt.Println("4")
+	<-c   // 主协程 等待
+	fmt.Println("5")
 }
