79fbfb0e6159667e1939bcba9c2958ddb04e5bb8
timeloveboy <734991033@qq.com>
2016-08-18 17:54:58 +0800

    ~

--- 基本语言知识/chan与goroutine/协程间通信/协程间通信.go
+++ 基本语言知识/chan与goroutine/协程间通信/协程间通信.go
@@ -1,9 +1,34 @@
 package main
 
-type msg struct{
+import (
+	"time"
+	"fmt"
+)
+var (sleeptime=time.Second*1)
+type Msg struct{
 	chanid int
 	data string
 }
-func main() {
 
+func Sender(msg chan Msg){
+	count:=0
+	for true{
+		count++
+		time.Sleep(sleeptime)
+		msg<-Msg{count,time.Now().String()[0:19]}
+	}
+}
+
+func Receiver(msg chan Msg)  {
+	for true {
+		m := <-msg
+		fmt.Println(m)
+	}
+}
+func main() {
+	Message:=make(chan Msg)
+	go Sender(Message)
+	go Receiver(Message)
+
+	for true{}
 }
