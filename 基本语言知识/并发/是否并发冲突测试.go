f3c17d6c03bb4109035aa8d465a2dac898de0375
timeloveboy <734991033@qq.com>
2016-09-08 15:24:35 +0800

    并发冲突

---
+++ 基本语言知识/并发/是否并发冲突测试.go
@@ -0,0 +1,29 @@
+// 会导致并发冲突的map访问
+// fatal error: concurrent map writes
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	m := make(map[int]string)
+	var closures [3]func(int)
+	for i := 0; i < 3; i++ {
+		closures[i] = func(v int) {
+			for {
+				m[v] += strconv.Itoa(v)
+				if len(m[v])%1000 == 0 {
+					fmt.Println(v, len(m[v]))
+				}
+			}
+		}
+	}
+	go closures[0](0)
+	go closures[1](1)
+	go closures[2](2)
+	for {
+
+	}
+}
